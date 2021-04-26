d86fe17e5b4da0be5b051d9f0912a613dd1858b2
coding-fantastic <alexnzioka@protonmail.com>
2021-04-26 05:58:36 +0300

    update

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask  , render_template , request , redirect , url_for
 app = Flask(__name__)
 
 
-@app.route('/helloworld')
-def helloworld():
-   return render_template('hello.html', myString="Jinja is awesome!!!")
+@app.route('/set1/a')
+def set1a():
+   return render_template('/set1/a.html', myString="Jinja is awesome!!!")
+
+@app.route('/set1/b')
+def set1b():
+   return render_template('/set1/b.html')
+
+@app.route('/set1/c')
+def set1c():
+   return render_template('/set1/c.html')
+
 
 if __name__ =="__main__":
     app.run(debug=True)
