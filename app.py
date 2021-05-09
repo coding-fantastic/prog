3620f81e99843077b78a77873fe48efa0d6e7aa6
coding-fantastic <alexnzioka@protonmail.com>
2021-05-09 23:48:54 +0300

    calling function in same directory and passing data from form to  its functon

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask  , render_template , request , redirect , url_for
+from main2 import ach
 app = Flask(__name__)
 
 
-@app.route('/set1/a')
+@app.route("/")
+def home():
+   return redirect(url_for("set1a"))
+
+@app.route('/set1/a', methods=["POST","GET"])
 def set1a():
-   return render_template('/set1/a.html', myString="Jinja is awesome!!!")
+   if request.method =="POST":
+      
+      tempans = request.form["query1"]
+
+      temp = "working alone"
+      temp = ach(tempans)
+
+      print (temp)
+      return render_template('/set1/b.html', ans=temp)
+      # return redirect(url_for("set1b", ans=temp))
+
 
 @app.route('/set1/b')
 def set1b():
@@ -13,6 +28,8 @@ def set1b():
 @app.route('/set1/c')
 def set1c():
    return render_template('/set1/c.html')
+
+
 
 
 if __name__ =="__main__":
