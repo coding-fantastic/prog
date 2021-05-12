04aa8958c23fc0eb952ca1c2fe75dfedaa235ff8
coding-fantastic <alexnzioka@protonmail.com>
2021-05-12 23:57:03 +0300

    app updates : queue if greater than one do redirect  and all other function handling the queue and redirecting

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask  , render_template , request , redirect , url_for
 from main2 import ach
+from collections import deque
+
 app = Flask(__name__)
 
+finaltype =  deque()
 
 @app.route("/")
 def home():
@@ -16,19 +19,89 @@ def set1a():
       temp = "working alone"
       temp = ach(tempans)
 
-      print (temp)
-      return render_template('/set1/b.html', ans=temp)
+      if  "can you repeat that please" in temp:
+         return render_template('/set1/a.html', ans="can you please try again ")
+      else:
+
+         print("response => " + str(temp))
+         temp = temp[0]
+         if len(finaltype) != 1:
+            finaltype.clear()
+         finaltype.append(temp.upper())
+         print("\nqueue =>" + str(finaltype)+ "\n")         
+         return render_template('/set2/a.html')
       # return redirect(url_for("set1b", ans=temp))
+         # return redirect(url_for("set1a"))
+   return render_template('/set1/a.html')
 
 
-@app.route('/set1/b')
-def set1b():
-   return render_template('/set1/b.html')
+@app.route('/set2/a', methods=["POST","GET"])
+def set2():
+   if request.method =="POST":
+      
+      tempans = request.form["query2"]
 
-@app.route('/set1/c')
-def set1c():
-   return render_template('/set1/c.html')
+      temp = "working alone"
+      temp = ach(tempans)
 
+      if  "can you repeat that please" in temp:
+         return render_template('/set2/a.html', ans="can you please try again ")
+      else:
+         temp = temp[0]
+         if len(finaltype) != 1:
+            # finaltype.popleft()
+            return render_template('/set1/a.html', ans="queue was greater than one.")
+         finaltype.append(temp.upper())
+         print("\nqueue =>" + str(finaltype)+ "\n")         
+         return render_template('/set3/a.html')
+   return render_template('/set2/a.html')
+
+@app.route('/set3/a', methods=["POST","GET"])
+def set3():
+   if request.method =="POST":
+      
+      tempans = request.form["query3"]
+
+      temp = "working alone"
+      temp = ach(tempans)
+
+      if  "can you repeat that please" in temp:
+         return render_template('/set3/a.html', ans="can you please try again ")
+      else:
+         temp = temp[0]
+         if len(finaltype) != 2:
+            return render_template('/set1/a.html', ans="queue was greater than two.")
+         finaltype.append(temp.upper())
+         print("\nqueue =>" + str(finaltype)+ "\n")         
+         return render_template('/set4/a.html')
+   return render_template('/set3/a.html')
+
+@app.route('/set4/a', methods=["POST","GET"])
+def set4():
+   if request.method =="POST":
+      
+      tempans = request.form["query4"]
+
+      temp = "working alone"
+      temp = ach(tempans)
+
+      if  "can you repeat that please" in temp:
+         return render_template('/set4/a.html', ans="can you please try again ")
+      else:
+         temp = temp[0]
+         if len(finaltype) != 3:
+            return render_template('/set1/a.html', ans="queue was greater than three.")
+         finaltype.append(temp.upper())
+         print("\nqueue =>" + str(finaltype)+ "\n")         
+         f = finaltype[0] + finaltype[1] + finaltype[2] + finaltype[3]
+         return render_template('/final.html', type = f)
+   return render_template('/set4/a.html')
+
+
+@app.route('/final')
+def final():
+   f = finaltype[0] + finaltype[1] + finaltype[2] + finaltype[3]
+   return render_template('/final.html', type = f)
 
 
 
