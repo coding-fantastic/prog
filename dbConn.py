a29136667eb2b955aea9187aeefd635ee435485e
coding-fantastic <alexnzioka@protonmail.com>
2021-05-15 03:49:13 +0300

    handle  fetching of data from db

--- dbConn.py
+++ dbConn.py
@@ -9,10 +9,34 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-def readDb():
-    query = "SELECT * FROM `typeTbl` LIMIT 1 "
-    mycursor.execute(query)
-    for x in mycursor:
-        print(x)
+def readDb(abrr):
+    query = "SELECT * FROM `typeTbl`  "
+    
+    query2 = "SELECT typeTbl.typename ,typeTbl.typeabbr , careersTbl.careername, careersTbl.careerdef, careersTbl.schooloffering, careersTbl.coursename FROM typeTbl INNER JOIN careersTbl ON typeTbl.ttid = careersTbl.ttid WHERE typeTbl.typeabbr = '" + abrr+  "' "
+    query3 = "SELECT typeTbl.typeabbr , careersAvoidTbl.careername FROM typeTbl INNER JOIN careersAvoidTbl ON typeTbl.ttid = careersAvoidTbl.ttid WHERE typeTbl.typeabbr = '" + abrr+  "' "
+    
+    mycursor.execute(query2  )
+    typeTblAndCareersTblList = []
+    # mycursor.execute(query3)
+    for row in mycursor:
+        # print(row)
+        typeTblAndCareersTblList.append(row)
+    
+    mycursor.execute( query3 )
+    careersToAvoidList = []
+    # mycursor.execute(query3)
+    for row in mycursor:
+        # print(row)
+        careersToAvoidList.append(row)
 
-# readDb()
+
+    # print("\nthis is the list : schools offering this \n" + str(mylist[0][4]))
+    # return mylist[0][0],mylist[0][1],mylist[0][2],mylist[0][3],mylist[0][4],mylist[0][5],
+    return typeTblAndCareersTblList , careersToAvoidList
+        
+typeTblAndCareersTblList , careersToAvoidList = readDb("ISTJ")
+print("schools offereing " + typeTblAndCareersTblList[3][5] + " are " + typeTblAndCareersTblList[3][4] )
+print("schools offereing " + typeTblAndCareersTblList[0][5] + " are " + typeTblAndCareersTblList[0][4] )
+# print("schools offering " + typeTblAndCareersTblList[0][6] + " are " + typeTblAndCareersTblList[0][5])
+
+    
